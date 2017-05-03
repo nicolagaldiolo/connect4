b3a08933c9fbf9f29882e81c8c8719cf5db3b732
Luciano Melotti <luciano.melotti@kleis.it>
2017-05-03 21:29:12 +0200

    deleted console.log

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -4,10 +4,8 @@ import Column from '../Column/Column';
 
 class Board extends Component {
   render() {
-    console.log(this.props);
     const columns = [];
     for(let i = 0; i < Number.parseInt(this.props.cols, 10); i++){
-      console.log(i);
       columns.push(<Column key={i} cells="7"/>) 
     }
     return (
