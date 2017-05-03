934c04faa857d47ab6c418273ef09033124d1616
Nicola Galdiolo <galdiolo.nicola@gmail.com>
2017-05-03 20:48:48 +0200

    Add Column Component

---
+++ src/Column/Column.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Column.css';
+
+class Column extends Component {
+  render() {
+    return (
+      <div className="Column">
+        <h2>Hi</h2>
+      </div>
+    );
+  }
+}
+
+export default Column;
